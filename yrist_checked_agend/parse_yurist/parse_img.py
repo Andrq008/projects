6c5e77388f43c9170eeb989751776160f0d29f96
Andrey Seligenenko <andriik00@gmail.com>
2023-01-10 15:07:22 +0300

    vse

--- yrist_checked_agend/parse_yurist/parse_img.py
+++
@@ -1,8 +0,0 @@
-import base64
-import io
-from PIL import Image
-
-def down_img(src):
-    b = "%s" % src
-    z = b[b.find('/9'):]
-    return Image.open(io.BytesIO(base64.b64decode(z))).save('/home/specit/parse_yurist/test.jpg')
